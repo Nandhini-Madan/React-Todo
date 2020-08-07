6adb8b0a17762691188b6a65a7ee99cf876b9d6e
unknown <nandhinimadan20@gmail.com>
2020-08-06 18:54:15 -0700

    App completed

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -2,10 +2,11 @@ import React from "react";
 
 const Todo =props=>{
     return(
-        <div
-            className={`list${props.list.completed ? "completed":" "}`}
-            onClick={()=>{props.toggleList(props.list.id)}}>
-            <p>{props.list.task}</p>
+        <div className="allign">
+            
+            <p className={`list ${props.list.completed ? "completed":" "}`}>{props.list.task}</p>
+            
+            <button className="donebtn" onClick={()=>{props.toggleList(props.list.id)}}>Done</button>
 
         </div>
     )
